8180eead980566df0d6e8cfe8f0f0444a8a966ec
leejw05 <zeta050525@gmail.com>
2022-02-13 10:39:50 +0900

    Create Getinfo.py

---
+++ GetInfo.py
@@ -0,0 +1,23 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+class GetInfo:
+    def __init__(self,url):
+        self.userAgent = 'ozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
+        self.headers = {'User-Agent': self.userAgent}
+        self.response = requests.get(url,headers=self.headers)
+        self.html = self.response.content
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+
+    def FindInfos(self):
+        self.Names = self.soup.find_all("div","name")
+        self.imgUrls = self.soup.find_all("img","search-product-wrap-img")
+        self.Grades = self.soup.find_all("em","rating")
+        self.GradeCount = self.soup.find_all("span","rating-total-count")
+
+    def ShowInfos(self):
+        print(self.Names[1].text)
+        print(self.imgUrls[1]['src'])
+        print(self.Grades[1].text)
+        print(self.GradeCount[1].text)
--- index.py
+++ index.py
@@ -1,25 +1,4 @@
-from mimetypes import init
-from re import S
-import bs4
-import requests
-from bs4 import BeautifulSoup
-class GetInfo:
-    def __init__(self,url):
-        self.userAgent = 'ozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
-        self.headers = {'User-Agent': self.userAgent}
-        self.response = requests.get(url,headers=self.headers)
-        self.html = self.response.content
-        self.soup = BeautifulSoup(self.html, 'html.parser')
-
-    def FindInfos(self):
-        self.Names = self.soup.find_all("div","name")
-        self.imgUrls = self.soup.find_all("div","search-product-wrap-img")
-        self.Grades = self.soup.find_all("div","rating")
-        self.GradeCount = self.soup.find_all("div","rating-total-count")
-
-    def ShowInfos(self):
-        print(self.Names)
-    # def Showinfos(self):
+from GetInfo import GetInfo
 
 
 Bs4 = GetInfo('https://www.coupang.com/np/search?component=&q=%EB%A7%88%EC%9A%B0%EC%8A%A4&channel=user')
